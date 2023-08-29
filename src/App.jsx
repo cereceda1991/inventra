8d70ec7b5dfe29ab26e07e061d8188bc4364d3f6
cereceda1991 <cereceda1991@gmail.com>
2023-08-29 01:22:54 -0500

    feat: add component sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
 
   return (
     <main>
       <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="/Register" element={<Register />} />
-          </Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
+      </Routes>
     </main>
   )
 }
