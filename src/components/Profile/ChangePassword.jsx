33b96e139ce26e3d9fc3ccd9afceff15e006b460
cereceda1991 <cereceda1991@gmail.com>
2023-09-08 21:05:40 -0500

    feat: Add password form update

--- src/components/Profile/ChangePassword.jsx
+++ src/components/Profile/ChangePassword.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { BiShow, BiHide } from 'react-icons/bi';
 import ButtonGeneric from '../ButtonGeneric/ButtonGeneric';
 import './ChangePassword.css';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import showDialog from '../../utils/showDialog';
+import { updateUser } from '../../Redux/User/userActions';
 
 function ChangePassword() {
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+  const [userId, setUserId] = useState(null);
+  const [errors, setErrors] = useState({
+    password: '',
+  });
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Carga los datos almacenados desde Local Storage cuando el componente se monta
+    const userResponseString = localStorage.getItem('userResponse');
+    if (userResponseString) {
+      const userResponse = JSON.parse(userResponseString);
+      setUserId(userResponse.data.user._id);
+    }
+  }, []);
+
+  // Calcula si el botón "Guardar" debe estar deshabilitado
   const isDisabled = !newPassword || !confirmPassword;
 
   const handleNewPasswordChange = (event) => {
@@ -24,16 +45,56 @@ function ChangePassword() {
     setShowPassword(!showPassword);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (newPassword === confirmPassword) {
-      // Aquí puedes implementar la lógica para cambiar la contraseña.
-      toast.success('Contraseña cambiada exitosamente');
-    } else {
-      toast.error('Las contraseñas no coinciden');
+    // Muestra un cuadro de diálogo de confirmación
+    const confirmed = await showDialog(
+      'Confirmar Acción',
+      '¿Estás seguro de que deseas realizar esta acción? Después de completarla, deberás volver a iniciar sesión con tus nuevas credenciales.',
+      'warning',
+      '#FF5733',
+    );
+
+    if (confirmed) {
+      if (newPassword === confirmPassword) {
+        // Datos del usuario actualizados
+        const updatedUserData = {
+          password: newPassword, // Usar newPassword en lugar de event.target.password.value
+        };
+
+        try {
+          // Realiza una solicitud para cambiar la contraseña utilizando Redux
+          const response = await dispatch(updateUser(userId, updatedUserData));
+
+          if (response) {
+            toast.success('Contraseña cambiada exitosamente');
+          } else {
+            toast.error('Las contraseñas no coinciden');
+          }
+
+          // Elimina los datos de inicio de sesión del almacenamiento local
+          localStorage.removeItem('userResponse');
+          localStorage.removeItem('authToken');
+
+          // Redirige al usuario a la página de inicio
+          navigate('/');
+        } catch (error) {
+          console.error('Error al cambiar la contraseña:', error);
+          setErrors({
+            password: error.error?.password || '',
+          });
+          toast.error('Error al cambiar la contraseña');
+        }
+      } else {
+        toast.error('Las contraseñas no coinciden');
+      }
     }
   };
+
+  // Define la clase CSS para resaltar los campos con error
+  const inputErrorClass = errors.password ? 'input-error' : '';
+
 
   return (
     <main className="container__change-password">
@@ -47,6 +108,7 @@ function ChangePassword() {
               value={newPassword}
               onChange={handleNewPasswordChange}
               autoComplete="new-password"
+              className={inputErrorClass}
               required
             />
             {showPassword ? (
@@ -68,6 +130,7 @@ function ChangePassword() {
               value={confirmPassword}
               onChange={handleConfirmPasswordChange}
               autoComplete="new-password"
+              className={inputErrorClass}
               required
             />
             {showPassword ? (
@@ -82,7 +145,8 @@ function ChangePassword() {
               />
             )}
           </div>
-          <ButtonGeneric buttonContent="Guardar" isDisabled={isDisabled} />
+          {errors.password && <div className="changePassword__error">{errors.password}</div>}
+          <ButtonGeneric type='submit' buttonContent="Guardar" isDisabled={isDisabled} />
         </form>
       </section>
     </main>
